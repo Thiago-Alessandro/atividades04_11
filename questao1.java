bc2d6e7634cd75eb0fb5b979294215609d663354
THIAGO ALESSANDRO BATISTA <thiago_a_batista@estudante.sesisenai.org.br>
2022-11-04 15:18:37 -0300

    opa

---
+++ questao1.java
@@ -0,0 +1,52 @@
+package atividade04_11;
+
+import java.util.Scanner;
+
+public class questao1 {
+
+	public static void main(String[] args) {
+		
+		double[] totalVendas = new double[10], valorReceber = new double[10],percentualComissao = new double[10];
+		double maiorValor = 0, menorValor = 0, totalVendasVendedores = 0;
+		int c = 0, c2 = 0;
+		
+		String [] vendedor = new String [10];
+		
+		String nomeMaiorValorReceber = new String();
+		String nomeMenorValorReceber = new String();
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		for (;c < 4;c++) {
+			
+			System.out.print("Insira o seu nome: ");
+			vendedor[c] = teclado.next();
+			
+			System.out.println("Insira o total de suas vendas: R$");
+			totalVendas[c] = teclado.nextDouble();
+			
+			System.out.print("Insira o seu percentual de comissão sobre as vendas: ");
+			percentualComissao[c] = teclado.nextDouble();
+			
+			valorReceber[c] = totalVendas[c] * percentualComissao[c]/100;
+			
+			totalVendasVendedores += valorReceber[c];
+			
+			 if (valorReceber[c] > maiorValor || c == 0) {
+				 maiorValor = valorReceber[c];
+				 nomeMaiorValorReceber = vendedor[c];
+			 }
+			 if (valorReceber[c] < menorValor || c == 0) {
+				 menorValor = valorReceber[c];
+				 nomeMenorValorReceber = vendedor[c];
+			 }
+		}
+		for (;c2 < vendedor.length;c2++) {
+		System.out.print("\n\nO(a) vendedor(a) " + vendedor[c2] + " receberá R$" + valorReceber[0] );
+		
+		System.out.print("\n\nO maior valor a receber é: R$" + maiorValor + " do vendedor " + nomeMaiorValorReceber);
+		System.out.print("\nO menor valor a receber é: R$" + menorValor + " do vendedor " + nomeMenorValorReceber);
+		}
+	}
+
+}
